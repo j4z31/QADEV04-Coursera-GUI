6626ab491b5ab017d8089d0f2afd409f0586a9ae
Jhasmany Quiroz <Jhasmany.Quiroz@fundacion-jala.org>
2015-11-13 11:16:00 -0400

    Implement the SearchCourseSteps class for execute a search of a course

---
+++ src/test/java/steps/SearchCourseSteps.java
@@ -0,0 +1,29 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: jhasmanyquiroz
+ * Date: 11/12/15
+ * Time: 3:02 PM
+ * To change this template use File | Settings | File Templates.
+ */
+package steps;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import ui.PageTransporter;
+import ui.pages.CoursesPage;
+import ui.pages.MainPage;
+
+public class SearchCourseSteps {
+    private static PageTransporter page = PageTransporter.getInstance();
+    private MainPage mainPage;
+    private CoursesPage coursesPage;
+
+    @Given("^I navigate to Main Page$")
+    public void navigateToMainPage() {
+        mainPage = page.navigateToMainPage();
+    }
+
+    @When("^I search a course as \"JavaScript\"$")
+    public  void searchACourseAs() {
+    }
+}
