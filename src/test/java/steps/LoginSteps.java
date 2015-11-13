5bf8c399c4215771e3459cd4cf3a86b71fcb997e
Jhasmany Quiroz <Jhasmany.Quiroz@fundacion-jala.org>
2015-11-13 11:13:15 -0400

    Implement the methods for the asserts

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -11,21 +11,32 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import ui.PageTransporter;
+import ui.pages.HomePage;
+import ui.pages.LoginPage;
+import ui.pages.MainPage;
+
+import static org.testng.Assert.assertTrue;
 
 public class LoginSteps {
-    public PageTransporter page = PageTransporter.getInstance();
+    private static PageTransporter page = PageTransporter.getInstance();
+    private HomePage homePage;
+    private LoginPage loginPage;
+    private MainPage mainPage;
 
     @Given("^I navigate to Login page$")
     public void navigateLoginPage(){
-        page.navigateToMainPage();
+        mainPage = page.navigateToMainPage();
+        loginPage = mainPage.clickLogInButton();
+        //loginPage = page.navigateToLoginPage();
     }
 
     @When("^I login as \"(.*?)\" with password \"(.*?)\"$")
     public void loginAs(String userName, String userPassword){
-        page.navigateToLoginPage();
+        homePage = loginPage.loginSuccessful(userName, userPassword);
     }
 
-    @Then("^I should login successfully$")
-    public void should_login_successfully(){
+    @Then("^I should login successfully.$")
+    public void shouldLoginSuccessfully(){
+        assertTrue(homePage.isPartnersDisplayed(), "User Name displayed");
     }
 }
