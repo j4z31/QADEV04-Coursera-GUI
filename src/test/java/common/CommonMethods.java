82396cf63fc156507c4ad48b3e051de1a6b4bc8d
j4z31 <tumael_777@hotmail.com>
2015-11-19 20:11:17 -0400

    Created the global methods for login and logout

---
+++ src/test/java/common/CommonMethods.java
@@ -0,0 +1,30 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: jhasmanyquiroz
+ * Date: 11/19/15
+ * Time: 6:38 PM
+ * To change this template use File | Settings | File Templates.
+ */
+package common;
+
+import ui.PageTransporter;
+
+public class CommonMethods {
+    private static PageTransporter pageTransporter = PageTransporter.getInstance();
+
+    public static void logOut() {
+        pageTransporter
+                .navigateToHomePage()
+                .clickAuthenticatedDropdownButton()
+                .clickSignOutButton()
+        ;
+    }
+
+    public static void logIn() {
+        pageTransporter
+                .navigateToMainPage()
+                .clickLogInButton()
+                .loginSuccessful("Jhasmany.Quiroz@fundacion-jala.org", "1c7hu57&7")
+        ;
+    }
+}
